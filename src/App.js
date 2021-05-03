9f15813766d06303578242f11291607d61480f97
imades <imad.elissaouii@gmail.com>
2021-05-03 23:31:53 +0000

    Full version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const API={
+  key:"c0a6af5b98a87a6deea8206f02f7e690",
+  base: "https://api.openweathermap.org/data/2.5/"
+
+}
 function App() {
+
+  const [query,setQuery]= useState('');
+  const [weather,setWeather]= useState({});
+  const search=evt=>{
+    if(evt.key==="Enter"){
+      fetch(`${API.base}weather?q=${query}&units=metric&APPID=${API.key}`)
+      .then(res => res.json() )
+      .then(result => setWeather(result));  
+    }
+  }
+  const dateBuilder=(d)=>{
+    let months=["January","February","March","April","May","June","July","August","September","October","November",
+  "December"];
+  let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  let day= days[d.getDay()];
+  let date=d.getDate();
+  let month=months[d.getMonth()];
+  let year=d.getFullYear();
+  return `${day} ${date} ${month} ${year}`
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+      <div className="col-sm-6 col-md-12 col-lg-12">
+        <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16)
+                        ? 'app warm':'app'):'app'}>
+      <main >
+      
+        <div className="search-box">
+          <input type="text" className="search-bar" 
+          onChange={e =>setQuery(e.target.value)}
+          value={query}
+          onKeyPress={search} 
+           placeholder="Search..."/>
+        </div>
+        {(typeof weather.main !="undefined")?(
+        <div>
+          <div className="location-box">
+          <div className="location">{weather.name},{weather.sys.country}</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">{Math.round(weather.main.temp)}°C</div>
+          <div className="weather">{weather.weather[0].main}</div>
+        </div>
+        </div>
+        ):('')}
+      </main>
     </div>
-  );
+ 
+    </div>
+  
+  
+   
+    )
 }
 
 export default App;
